fea24161fbde278acf1f76cd839297c00506b59b
PhoonThetPine <thetpine254@gmail.com>
2024-03-26 17:59:28 +0800

    Initial Commit

---
+++ BarcodeScanner App/BarcodeScanner_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScanner_AppApp.swift
+//  BarcodeScanner App
+//
+//  Created by Phoon Thet Pine on 26/3/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScanner_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
